8e286ce684c62a6d623c10691263450e4dd6d6b7
Akshay Wadhi <168399945+akshaywadhi@users.noreply.github.com>
2025-05-23 12:22:43 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,18 +5,31 @@ import { router } from "./routers/adminRouter.js";
 import { userRouter } from "./routers/userRouter.js";
 import cors from "cors";
 
-const app = express();
 dotenv.config();
-const port = process.env.PORT
+
+const app = express();
+const port = process.env.PORT;
+
 app.use(express.json());
 app.use(cors({
   origin: "https://emsfrontend-phi.vercel.app",
   credentials: true,
 }));
+
 app.use("/", router);
 app.use("/user", userRouter);
 
-app.listen(port, () => {
-  console.log("Server Is Running On Port",port);
-  connectDB();
-});
+
+async function startServer() {
+  try {
+    await connectDB(); 
+    app.listen(port, () => {
+      console.log("Server Is Running On Port", port);
+    });
+  } catch (error) {
+    console.error("Failed to connect to DB:", error);
+    process.exit(1); 
+  }
+}
+
+startServer();
